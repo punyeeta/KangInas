f2a1d01be2b59d8f10a8561f181c2589269cc1eb
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2025-03-13 16:09:11 +0800

    ambot ani yawa

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
+from cloudinary.forms import CloudinaryFileField
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
-    profile_picture = serializers.ImageField(required=False)
+    # Don't explicitly define profile_picture here, let it be handled by the model
     
     class Meta:
         model = User
